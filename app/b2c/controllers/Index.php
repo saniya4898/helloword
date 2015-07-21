a28936164c417861c37b37c4b55a154c35ede0b7
letwang <498936940@qq.com>
2015-07-21 15:16:31 +0800

    Update Index.php

--- app/b2c/controllers/Index.php
+++ app/b2c/controllers/Index.php
@@ -10,6 +10,7 @@ class IndexController extends Yaf_Controller_Abstract
 
     public function indexAction()
     {
+        echo Yaf_Registry::get('I18n')->get('payment', 'I have %num% cat and the value is %value%.', array('%num%' => '19', '%value%' => '15'));
         $this->_view->word = "hello world";
     }
 }

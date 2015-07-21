fb5cb0bb5c03ff41056b8d5fb9488d772077f0d3
letwang <498936940@qq.com>
2015-07-21 15:46:18 +0800

    Update Bootstrap.php

--- app/b2c/Bootstrap.php
+++ app/b2c/Bootstrap.php
@@ -10,19 +10,20 @@ class Bootstrap extends Yaf_Bootstrap_Abstract
         
         // auto load config data
         $this->config = Yaf_Application::app()->getConfig();
-        Yaf_Registry::set('config', $this->config);
+        Yaf_Registry::set('Config', $this->config);
         
         //auto load redis
         $redis = new Redis();
         $redis->connect($this->config->redis->host, $this->config->redis->port, $this->config->redis->timeout, $this->config->redis->reserved, $this->config->redis->interval);
-        Yaf_Registry::set('redis', $redis);
+        Yaf_Registry::set('Redis', $redis);
         
         //auto load mysql
-        Yaf_Registry::set('dbRead', new Db ( 'mysql:host=' . $this->config->mysql->read->host . ';dbname=' . $this->config->mysql->read->dbname . ';charset=' . $this->config->mysql->read->charset . ';port=' . $this->config->mysql->read->port . '', $this->config->mysql->read->username, $this->config->mysql->read->password));
-        Yaf_Registry::set('dbWrite', new Db ( 'mysql:host=' . $this->config->mysql->write->host . ';dbname=' . $this->config->mysql->write->dbname . ';charset=' . $this->config->mysql->write->charset . ';port=' . $this->config->mysql->write->port . '', $this->config->mysql->write->username, $this->config->mysql->write->password));
+        Yaf_Registry::set('DbRead', new Db ( 'mysql:host=' . $this->config->mysql->read->host . ';dbname=' . $this->config->mysql->read->dbname . ';charset=' . $this->config->mysql->read->charset . ';port=' . $this->config->mysql->read->port . '', $this->config->mysql->read->username, $this->config->mysql->read->password));
+        Yaf_Registry::set('DbWrite', new Db ( 'mysql:host=' . $this->config->mysql->write->host . ';dbname=' . $this->config->mysql->write->dbname . ';charset=' . $this->config->mysql->write->charset . ';port=' . $this->config->mysql->write->port . '', $this->config->mysql->write->username, $this->config->mysql->write->password));
         
         // auto load model
         Yaf_Registry::set('I18n', new I18nModel($this->config->application->name, 'cn'));
+        Yaf_Registry::set('Cache', new CacheModel($this->config->application->name, 'cn'));
         
         // auto load plugin
         $dispatcher->registerPlugin(new GlobalPlugin());

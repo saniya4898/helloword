086a9a93f616a8df1148dede54e538e783a00949
letwang <498936940@qq.com>
2015-07-21 15:17:47 +0800

    Update I18n.php

--- app/b2c/models/I18n.php
+++ app/b2c/models/I18n.php
@@ -1,37 +1,47 @@
 <?php
-class I18nModel{
-	private $app;
-	private $lang;
+
+class I18nModel
+{
+
+    private $app;
+
+    private $lang;
 
     private $key;
+
     private $fieldMd5;
 
-    public function __construct($app, $lang) {
+    public function __construct($app, $lang)
+    {
         $this->app = $app;
         $this->lang = $lang;
     }
-    
-    public function get($module, $field, array $value = array()) {
-        $this->key = $this->app.':lang:'.$this->lang.':'.$module;
-        $this->fieldMd5 = md5($field);
 
-        if($this->exists($this->key, $this->fieldMd5)){
+    public function get($module, $field, array $value = array())
+    {
+        $this->key = $this->app . ':lang:' . $this->lang . ':' . $module;
+        $this->fieldMd5 = md5($field);
+        
+        if ($this->exists($this->key, $this->fieldMd5)) {
             return strtr(Yaf_Registry::get('redis')->hget($this->key, $this->fieldMd5), $value);
         }
-
+        
         $this->add($this->key, $this->fieldMd5, $field);
         return strtr($field, $value);
     }
 
-    public function add($key, $fieldMd5, $field) {
+    public function add($key, $fieldMd5, $field)
+    {
         return Yaf_Registry::get('redis')->hset($key, $fieldMd5, $field);
     }
-    
-    public function exists($key, $fieldMd5) {
+
+    public function exists($key, $fieldMd5)
+    {
         return Yaf_Registry::get('redis')->hexists($key, $fieldMd5);
     }
 
-    public function delete($key, $fieldMd5) {
+    public function delete($key, $fieldMd5)
+    {
         return Yaf_Registry::get('redis')->hdel($key, $fieldMd5);
     }
 }

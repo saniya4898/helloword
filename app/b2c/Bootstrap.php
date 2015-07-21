4222260286fb32a5c483ea76d3549ae7cfdc7323
letwang <498936940@qq.com>
2015-07-21 13:31:00 +0800

    Update Bootstrap.php

--- app/b2c/Bootstrap.php
+++ app/b2c/Bootstrap.php
@@ -2,24 +2,24 @@
 
 class Bootstrap extends Yaf_Bootstrap_Abstract
 {
-
+    private $config;
     public function _init(Yaf_Dispatcher $dispatcher)
     {
         // auto start session
         Yaf_Session::getInstance()->start();
         
         // auto load config data
-        $config = Yaf_Application::app()->getConfig();
-        Yaf_Registry::set('config', $config);
+        $this->config = Yaf_Application::app()->getConfig();
+        Yaf_Registry::set('config', $this->config);
         
         //auto load redis
         $redis = new Redis();
-        $redis->connect($config->redis->host, $config->redis->port, $config->redis->timeout, $config->redis->reserved, $config->redis->interval);
+        $redis->connect($this->config->redis->host, $this->config->redis->port, $this->config->redis->timeout, $this->config->redis->reserved, $this->config->redis->interval);
         Yaf_Registry::set('redis', $redis);
         
         //auto load mysql
-        Yaf_Registry::set('dbRead', new Db ( 'mysql:host=' . $config->mysql->read->host . ';dbname=' . $config->mysql->read->dbname . ';charset=' . $config->mysql->read->charset . ';port=' . $config->mysql->read->port . '', $config->mysql->read->username, $config->mysql->read->password));
-        Yaf_Registry::set('dbWrite', new Db ( 'mysql:host=' . $config->mysql->write->host . ';dbname=' . $config->mysql->write->dbname . ';charset=' . $config->mysql->write->charset . ';port=' . $config->mysql->write->port . '', $config->mysql->write->username, $config->mysql->write->password));
+        Yaf_Registry::set('dbRead', new Db ( 'mysql:host=' . $this->config->mysql->read->host . ';dbname=' . $this->config->mysql->read->dbname . ';charset=' . $this->config->mysql->read->charset . ';port=' . $this->config->mysql->read->port . '', $this->config->mysql->read->username, $this->config->mysql->read->password));
+        Yaf_Registry::set('dbWrite', new Db ( 'mysql:host=' . $this->config->mysql->write->host . ';dbname=' . $this->config->mysql->write->dbname . ';charset=' . $this->config->mysql->write->charset . ';port=' . $this->config->mysql->write->port . '', $this->config->mysql->write->username, $this->config->mysql->write->password));
         
         // auto load global model
         Yaf_Registry::set('model', new GlobalModel());
